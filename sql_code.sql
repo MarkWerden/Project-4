029509eb19dfe56c79aab8398aea72f0abf023a5
Elza Hayyat <ekhayat@g.ucla.edu>
2022-06-16 18:22:32 -0700

    Postgres storage code  added

---
+++ sql_code.sql
@@ -0,0 +1,18 @@
+CREATE TABLE loan_data (
+	Loan_ID	VARCHAR,
+	Gender VARCHAR,
+	Married VARCHAR,
+	Dependents VARCHAR,
+	Education VARCHAR,
+	Self_Employed VARCHAR,
+	ApplicantIncome VARCHAR,
+	CoapplicantIncome VARCHAR,
+	LoanAmount VARCHAR,
+	Loan_Amount_Term VARCHAR,
+	Credit_History VARCHAR,
+	Property_Area VARCHAR,
+	Loan_Status VARCHAR
+	
+);
+
+SELECT * from loan_data;
